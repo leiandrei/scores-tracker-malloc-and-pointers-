8e5032f29a81acc7d6f9f1be4a1139902fda695b
leiandrei <leiandreiedomaoal@iskolarngbayan.pup.edu.ph>
2025-05-04 16:52:25 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void behind(int *s, int size);
+
+int main()
+{
+    int size;
+
+    printf("Enter the number of playeres (max. 10): ");
+    scanf("%d", &size);
+
+    if (size > 10 || size <= 0) {
+        printf("\nInvalid Input!\n");
+        return 1;
+    }
+
+    int *s = calloc(size, sizeof(int));
+
+    if (s == NULL) {
+        fprintf(stderr, "Memory Allocation failed!\n");
+        return 1;
+    }
+
+    printf("Enter the scores for each players:\n");
+    for (int i = 0; i < size; i++) {
+        printf("Player #%d: ", i + 1);
+        scanf("%d", &s[i]);
+    }
+
+    behind(s, size);
+
+    printf("Points behind the leader:\n");
+    for (int i = 0; i < size; i++) {
+        printf("Player #%d: %d\n", i + 1, s[i]);
+    }
+
+    free(s);
+
+
+    return 0;
+}
+
+void behind(int *a, int size)
+{
+    int high = 0;
+    for (int i = 0; i < size; i++) {
+        if (*(a+i) > high) {
+            high = *(a+i);
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        *(a + i) = high - *(a + i);
+    }
+}
